55d45556c401426ec76d26628a2c3264b9183765
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-10-03 14:14:27 -0700

    added eating sounds

---
+++ eating-sound.lua
@@ -0,0 +1,19 @@
+local sounds = {}
+
+sounds.eat_apple_sound =
+{
+  {
+    filename = "__factorio-apples__/sounds/eat1.ogg",
+    volume = 0.6
+  },
+  {
+    filename = "__factorio-apples__/sounds/eat2.ogg",
+    volume = 0.6
+  },
+  {
+    filename = "__factorio-apples__/sounds/eat3.ogg",
+    volume = 0.6
+  }
+}
+
+return sounds
