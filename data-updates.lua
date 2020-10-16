f3d5e0acdd1c68618dacf947a2c3586b2a78cca5
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-10-16 10:47:09 -0700

    add apples to tree mining results instead of overwriting them

--- data-updates.lua
+++ data-updates.lua
@@ -1,104 +1,179 @@
-data.raw.tree["tree-01"].minable.result = nil
-data.raw.tree["tree-01"].minable.count = nil
-data.raw.tree["tree-01"].minable.results = {
-  {type="item", name="wood", probability=1, amount=4},
-  {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
-}
+table.insert(data.raw.tree["tree-01"].minable.results, {
+  {type = "item", name = "wood", probability = 1, amount = 4},
+  {type = "item", name = "apple", probability=0.16, amount_min=0, amount_max=3}
+})
 
-data.raw.tree["tree-02"].minable.result = nil
-data.raw.tree["tree-02"].minable.count = nil
-data.raw.tree["tree-02"].minable.results = {
-  {type="item", name="wood", probability=1, amount=4},
-  {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
-}
+table.insert(data.raw.tree["tree-02"].minable.results, {
+  {type = "item", name = "wood", probability = 1, amount = 4},
+  {type = "item", name = "apple", probability=0.16, amount_min=0, amount_max=3}
+})
 
-data.raw.tree["tree-02-red"].minable.result = nil
-data.raw.tree["tree-02-red"].minable.count = nil
-data.raw.tree["tree-02-red"].minable.results = {
-  {type="item", name="wood", probability=1, amount=4},
-  {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
-}
+table.insert(data.raw.tree["tree-02-red"].minable.results, {
+  {type = "item", name = "wood", probability = 1, amount = 4},
+  {type = "item", name = "apple", probability=0.16, amount_min=0, amount_max=3}
+})
 
-data.raw.tree["tree-03"].minable.result = nil
-data.raw.tree["tree-03"].minable.count = nil
-data.raw.tree["tree-03"].minable.results = {
-  {type="item", name="wood", probability=1, amount=4},
-  {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
-}
+table.insert(data.raw.tree["tree-03"].minable.results, {
+  {type = "item", name = "wood", probability = 1, amount = 4},
+  {type = "item", name = "apple", probability=0.16, amount_min=0, amount_max=3}
+})
 
-data.raw.tree["tree-04"].minable.result = nil
-data.raw.tree["tree-04"].minable.count = nil
-data.raw.tree["tree-04"].minable.results = {
-  {type="item", name="wood", probability=1, amount=4},
-  {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
-}
+table.insert(data.raw.tree["tree-04"].minable.results, {
+  {type = "item", name = "wood", probability = 1, amount = 4},
+  {type = "item", name = "apple", probability=0.16, amount_min=0, amount_max=3}
+})
 
-data.raw.tree["tree-05"].minable.result = nil
-data.raw.tree["tree-05"].minable.count = nil
-data.raw.tree["tree-05"].minable.results = {
-  {type="item", name="wood", probability=1, amount=4},
-  {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
-}
+table.insert(data.raw.tree["tree-05"].minable.results, {
+  {type = "item", name = "wood", probability = 1, amount = 4},
+  {type = "item", name = "apple", probability=0.16, amount_min=0, amount_max=3}
+})
 
-data.raw.tree["tree-06"].minable.result = nil
-data.raw.tree["tree-06"].minable.count = nil
-data.raw.tree["tree-06"].minable.results = {
-  {type="item", name="wood", probability=1, amount=4},
-  {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
-}
+table.insert(data.raw.tree["tree-06"].minable.results, {
+  {type = "item", name = "wood", probability = 1, amount = 4},
+  {type = "item", name = "apple", probability=0.16, amount_min=0, amount_max=3}
+})
 
-data.raw.tree["tree-06-brown"].minable.result = nil
-data.raw.tree["tree-06-brown"].minable.count = nil
-data.raw.tree["tree-06-brown"].minable.results = {
-  {type="item", name="wood", probability=1, amount=4},
-  {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
-}
+table.insert(data.raw.tree["tree-06-brown"].minable.results, {
+  {type = "item", name = "wood", probability = 1, amount = 4},
+  {type = "item", name = "apple", probability=0.16, amount_min=0, amount_max=3}
+})
 
-data.raw.tree["tree-07"].minable.result = nil
-data.raw.tree["tree-07"].minable.count = nil
-data.raw.tree["tree-07"].minable.results = {
-  {type="item", name="wood", probability=1, amount=4},
-  {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
-}
+table.insert(data.raw.tree["tree-07"].minable.results, {
+  {type = "item", name = "wood", probability = 1, amount = 4},
+  {type = "item", name = "apple", probability=0.16, amount_min=0, amount_max=3}
+})
 
-data.raw.tree["tree-08"].minable.result = nil
-data.raw.tree["tree-08"].minable.count = nil
-data.raw.tree["tree-08"].minable.results = {
-  {type="item", name="wood", probability=1, amount=4},
-  {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
-}
+table.insert(data.raw.tree["tree-08"].minable.results, {
+  {type = "item", name = "wood", probability = 1, amount = 4},
+  {type = "item", name = "apple", probability=0.16, amount_min=0, amount_max=3}
+})
 
-data.raw.tree["tree-08-brown"].minable.result = nil
-data.raw.tree["tree-08-brown"].minable.count = nil
-data.raw.tree["tree-08-brown"].minable.results = {
-  {type="item", name="wood", probability=1, amount=4},
-  {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
-}
+table.insert(data.raw.tree["tree-08-brown"].minable.results, {
+  {type = "item", name = "wood", probability = 1, amount = 4},
+  {type = "item", name = "apple", probability=0.16, amount_min=0, amount_max=3}
+})
 
-data.raw.tree["tree-08-red"].minable.result = nil
-data.raw.tree["tree-08-red"].minable.count = nil
-data.raw.tree["tree-08-red"].minable.results = {
-  {type="item", name="wood", probability=1, amount=4},
-  {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
-}
+table.insert(data.raw.tree["tree-08-red"].minable.results, {
+  {type = "item", name = "wood", probability = 1, amount = 4},
+  {type = "item", name = "apple", probability=0.16, amount_min=0, amount_max=3}
+})
 
-data.raw.tree["tree-09"].minable.result = nil
-data.raw.tree["tree-09"].minable.count = nil
-data.raw.tree["tree-09"].minable.results = {
-  {type="item", name="wood", probability=1, amount=4},
-  {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
-}
+table.insert(data.raw.tree["tree-09"].minable.results, {
+  {type = "item", name = "wood", probability = 1, amount = 4},
+  {type = "item", name = "apple", probability=0.16, amount_min=0, amount_max=3}
+})
 
-data.raw.tree["tree-09-brown"].minable.result = nil
-data.raw.tree["tree-09-brown"].minable.count = nil
-data.raw.tree["tree-09-brown"].minable.results = {
-  {type="item", name="wood", probability=1, amount=4},
-  {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
-}
+table.insert(data.raw.tree["tree-09-brown"].minable.results, {
+  {type = "item", name = "wood", probability = 1, amount = 4},
+  {type = "item", name = "apple", probability=0.16, amount_min=0, amount_max=3}
+})
 
-data.raw.tree["tree-09-red"].minable.result = nil
-data.raw.tree["tree-09-red"].minable.count = nil
-data.raw.tree["tree-09-red"].minable.results = {
-  {type="item", name="wood", probability=1, amount=4},
-  {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
-}
+table.insert(data.raw.tree["tree-09-red"].minable.results, {
+  {type = "item", name = "wood", probability = 1, amount = 4},
+  {type = "item", name = "apple", probability=0.16, amount_min=0, amount_max=3}
+})
+
+-- data.raw.tree["tree-01"].minable.result = nil
+-- data.raw.tree["tree-01"].minable.count = nil
+-- data.raw.tree["tree-01"].minable.results = {
+--   {type="item", name="wood", probability=1, amount=4},
+--   {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
+-- }
+--
+-- data.raw.tree["tree-02"].minable.result = nil
+-- data.raw.tree["tree-02"].minable.count = nil
+-- data.raw.tree["tree-02"].minable.results = {
+--   {type="item", name="wood", probability=1, amount=4},
+--   {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
+-- }
+--
+-- data.raw.tree["tree-02-red"].minable.result = nil
+-- data.raw.tree["tree-02-red"].minable.count = nil
+-- data.raw.tree["tree-02-red"].minable.results = {
+--   {type="item", name="wood", probability=1, amount=4},
+--   {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
+-- }
+--
+-- data.raw.tree["tree-03"].minable.result = nil
+-- data.raw.tree["tree-03"].minable.count = nil
+-- data.raw.tree["tree-03"].minable.results = {
+--   {type="item", name="wood", probability=1, amount=4},
+--   {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
+-- }
+--
+-- data.raw.tree["tree-04"].minable.result = nil
+-- data.raw.tree["tree-04"].minable.count = nil
+-- data.raw.tree["tree-04"].minable.results = {
+--   {type="item", name="wood", probability=1, amount=4},
+--   {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
+-- }
+--
+-- data.raw.tree["tree-05"].minable.result = nil
+-- data.raw.tree["tree-05"].minable.count = nil
+-- data.raw.tree["tree-05"].minable.results = {
+--   {type="item", name="wood", probability=1, amount=4},
+--   {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
+-- }
+--
+-- data.raw.tree["tree-06"].minable.result = nil
+-- data.raw.tree["tree-06"].minable.count = nil
+-- data.raw.tree["tree-06"].minable.results = {
+--   {type="item", name="wood", probability=1, amount=4},
+--   {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
+-- }
+--
+-- data.raw.tree["tree-06-brown"].minable.result = nil
+-- data.raw.tree["tree-06-brown"].minable.count = nil
+-- data.raw.tree["tree-06-brown"].minable.results = {
+--   {type="item", name="wood", probability=1, amount=4},
+--   {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
+-- }
+--
+-- data.raw.tree["tree-07"].minable.result = nil
+-- data.raw.tree["tree-07"].minable.count = nil
+-- data.raw.tree["tree-07"].minable.results = {
+--   {type="item", name="wood", probability=1, amount=4},
+--   {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
+-- }
+--
+-- data.raw.tree["tree-08"].minable.result = nil
+-- data.raw.tree["tree-08"].minable.count = nil
+-- data.raw.tree["tree-08"].minable.results = {
+--   {type="item", name="wood", probability=1, amount=4},
+--   {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
+-- }
+--
+-- data.raw.tree["tree-08-brown"].minable.result = nil
+-- data.raw.tree["tree-08-brown"].minable.count = nil
+-- data.raw.tree["tree-08-brown"].minable.results = {
+--   {type="item", name="wood", probability=1, amount=4},
+--   {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
+-- }
+--
+-- data.raw.tree["tree-08-red"].minable.result = nil
+-- data.raw.tree["tree-08-red"].minable.count = nil
+-- data.raw.tree["tree-08-red"].minable.results = {
+--   {type="item", name="wood", probability=1, amount=4},
+--   {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
+-- }
+--
+-- data.raw.tree["tree-09"].minable.result = nil
+-- data.raw.tree["tree-09"].minable.count = nil
+-- data.raw.tree["tree-09"].minable.results = {
+--   {type="item", name="wood", probability=1, amount=4},
+--   {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
+-- }
+--
+-- data.raw.tree["tree-09-brown"].minable.result = nil
+-- data.raw.tree["tree-09-brown"].minable.count = nil
+-- data.raw.tree["tree-09-brown"].minable.results = {
+--   {type="item", name="wood", probability=1, amount=4},
+--   {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
+-- }
+--
+-- data.raw.tree["tree-09-red"].minable.result = nil
+-- data.raw.tree["tree-09-red"].minable.count = nil
+-- data.raw.tree["tree-09-red"].minable.results = {
+--   {type="item", name="wood", probability=1, amount=4},
+--   {type="item", name="apple", probability=0.16, amount_min=0, amount_max=3}
+-- }
