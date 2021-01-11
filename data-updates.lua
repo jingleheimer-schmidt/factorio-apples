6fdcb25574436056f01826b9f930200bd5085c71
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2021-01-10 21:05:21 -0800

    rewrote how apple results are inserted into trees

--- data-updates.lua
+++ data-updates.lua
@@ -1,12 +1,32 @@
 for a,b in pairs(data.raw.tree) do
-  local apple_result = {type = "item", name = "apple", probability = 0.16, amount_min = 0, amount_max = 3}
-  -- if b.name == ("tree-01" or "tree-02" or "tree-02-red" or "tree-03" or "tree-04" or "tree-05" or "tree-06" or "tree-06-brown" or "tree-07" or "tree-08" or "tree-08-brown" or "tree-08-red" or "tree-09" or "tree-09-brown" or "tree-09-red") then
-    if not b.minable.results then
-      b.minable.results = {{type = "item", name = b.minable.result, amount = b.minable.count}, apple_result}
-    else
-      table.insert(b.minable.results, apple_result)
+  local apple_result = {type = "item", name = "apple", probability = 0.0, amount_min = 0, amount_max = 0}
+  local trees_to_add_apples_to = {
+    -- {name = "tree-01", probability = 0}
+    {name = "tree-02", probability = 0.2, amount_min = 0, amount_max = 4},
+    {name = "tree-02-red", probability = 0.4, amount_min = 1, amount_max = 8},
+    {name = "tree-03", probability = 0.18, amount_min = 0, amount_max = 3},
+    {name = "tree-04", probability = 0.1, amount_min = 0, amount_max = 6},
+    {name = "tree-05", probability = 0.3, amount_min = 2, amount_max = 5},
+    {name = "tree-07", probability = 0.2, amount_min = 0, amount_max = 4},
+    {name = "tree-08", probability = 0.2, amount_min = 1, amount_max = 4},
+    {name = "tree-08-brown", probability = 0.3, amount_min = 0, amount_max = 4},
+    {name = "tree-08-red", probability = 0.4, amount_min = 1, amount_max = 5},
+    {name = "tree-09", probability = 0.5, amount_min = 0, amount_max = 8},
+    {name = "tree-09-brown", probability = 0.6, amount_min = 0, amount_max = 8},
+    {name = "tree-09-red", probability = 0.6, amount_min = 0, amount_max = 8},
+  }
+  for c,d in pairs(trees_to_add_apples_to) do
+    if b.name == d.name then
+      apple_result.probability = apple_result.probability + d.probability
+      apple_result.amount_min = apple_result.amount_min + d.amount_min
+      apple_result.amount_max = apple_result.amount_max + d.amount_max
+      if ((not b.minable.results) and b.minable.result) then
+        b.minable.results = {{type = "item", name = b.minable.result, amount = b.minable.count}, apple_result}
+      else
+        table.insert(b.minable.results, apple_result)
+      end
     end
-  -- end
+  end
 end
 
 --
