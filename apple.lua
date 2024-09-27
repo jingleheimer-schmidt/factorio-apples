944e2f3a92e07691612b892b42e155e2fa498c00
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-09-27 10:28:11 -0700

    standardize formatting

--- apple.lua
+++ apple.lua
@@ -1,51 +1,52 @@
-local sounds = require ("eating-sound")
+
+local sounds = require("eating-sound")
 
 local apple = {
-  type = "capsule",
-  name = "apple",
-  icon = "__factorio-apples__/graphics/Apple_JE3_BE3.png",
-  icon_size = 160,
-  subgroup = "raw-resource",
-  capsule_action =
-  {
-    type = "use-on-self",
-    attack_parameters =
+    type = "capsule",
+    name = "apple",
+    icon = "__factorio-apples__/graphics/Apple_JE3_BE3.png",
+    icon_size = 160,
+    subgroup = "raw-resource",
+    capsule_action =
     {
-      type = "projectile",
-      activation_type = "consume",
-      ammo_category = "capsule",
-      cooldown = 16,
-      range = 0,
-      ammo_type =
-      {
-        category = "capsule",
-        target_type = "position",
-        action =
+        type = "use-on-self",
+        attack_parameters =
         {
-          type = "direct",
-          action_delivery =
-          {
-            type = "instant",
-            target_effects =
+            type = "projectile",
+            activation_type = "consume",
+            ammo_category = "capsule",
+            cooldown = 16,
+            range = 0,
+            ammo_type =
             {
-              {
-                type = "damage",
-                damage = {type = "physical", amount = -16}
-              },
-              {
-                type = "play-sound",
-                sound = sounds.eat_apple_sound,
-              },
+                category = "capsule",
+                target_type = "position",
+                action =
+                {
+                    type = "direct",
+                    action_delivery =
+                    {
+                        type = "instant",
+                        target_effects =
+                        {
+                            {
+                                type = "damage",
+                                damage = { type = "physical", amount = -16 }
+                            },
+                            {
+                                type = "play-sound",
+                                sound = sounds.eat_apple_sound,
+                            },
+                        }
+                    }
+                }
             }
-          }
         }
-      }
-    }
-  },
-  order = "h[raw-fish][apple]",
-  stack_size = 64
+    },
+    order = "h[raw-fish][apple]",
+    stack_size = 64
 }
 
 data:extend({
-  apple
+    apple
 })
--- eating-sound.lua
+++ eating-sound.lua
@@ -1,19 +1,20 @@
+
 local sounds = {}
 
 sounds.eat_apple_sound =
 {
-  {
-    filename = "__factorio-apples__/sounds/eat1.ogg",
-    volume = 0.6
-  },
-  {
-    filename = "__factorio-apples__/sounds/eat2.ogg",
-    volume = 0.6
-  },
-  {
-    filename = "__factorio-apples__/sounds/eat3.ogg",
-    volume = 0.6
-  }
+    {
+        filename = "__factorio-apples__/sounds/eat1.ogg",
+        volume = 0.6
+    },
+    {
+        filename = "__factorio-apples__/sounds/eat2.ogg",
+        volume = 0.6
+    },
+    {
+        filename = "__factorio-apples__/sounds/eat3.ogg",
+        volume = 0.6
+    }
 }
 
 return sounds
